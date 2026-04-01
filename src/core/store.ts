229aca25a50ccba89b2d504814a042d8542eed37
Ali <ajabowr@gmail.com>
2026-04-01 17:49:20 +0300

    Enhance log server functionality by adding stats endpoint and improving log entry handling

--- src/core/store.ts
+++ src/core/store.ts
@@ -9,11 +9,19 @@ export class Store {
     public addLogEntry(entry: LogEntry): void {
         this.logs.push(entry);
         this.totalLogEntries++; 
-        if(entry.level === LogLevel.ERROR) {
+        if(entry.level.toLocaleLowerCase() === LogLevel.ERROR) {
+        console.log(`entry level: ${entry.level}`);
+            
             this.errorCount++;
         }
     }
     public getLogs(): LogEntry[] {
         return this.logs;
+    }
+    public getStats(): { totalLogs: number; errorLogs: number } {
+        return {
+            totalLogs: this.totalLogEntries,
+            errorLogs: this.errorCount
+        }
     }
 }
--- src/server/http-server.ts
+++ src/server/http-server.ts
@@ -1,6 +1,7 @@
 
 import { Store } from "../core/store.js";
 import * as http from 'http';
+import { LogLevel } from "../types/log.types.js";
 export class LogServer {
     private port: number;
     private store: Store;
@@ -16,6 +17,19 @@ export class LogServer {
             if(req.url === '/logs' && req.method === 'GET'){
                 res.writeHead(200, {'Content-Type': 'application/json'});
                 res.end(JSON.stringify(this.store.getLogs()));
+                return;
+            }
+            if (req.url === '/stats' && req.method === 'GET') {
+                const logs = this.store.getLogs();
+                const totalLogs = logs.length;
+                const errorLogs = logs.filter(log => log.level === LogLevel.ERROR).length;
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify(this.store.getStats()));
+                return;
+            }
+            else {
+                res.writeHead(404, {'Content-Type': 'text/plain'});
+                res.end('Not Found');
             }
         });
         server.listen(this.port, () => {
